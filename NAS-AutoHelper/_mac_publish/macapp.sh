8d4c3204a027fae530cc751eec8b543f2faff0fa
bluesky <linsongxian@outlook.com>
2025-01-13 21:19:08 +0800

    macOS完美结束。

--- NAS-AutoHelper/_mac_publish/macapp.sh
+++ NAS-AutoHelper/_mac_publish/macapp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-APP_NAME="/Users/bluesky/Desktop/NAS-AutoHelper.app"
-PUBLISH_OUTPUT_DIRECTORY="/Users/bluesky/Workspace/Projects/No-Front/NAS-AutoHelper/NAS-AutoHelper/bin/Release/net8.0/osx-arm64/publish/."
+APP_NAME="./NAS-AutoHelper.app"
+PUBLISH_OUTPUT_DIRECTORY="../bin/Release/net8.0/osx-arm64/publish/."
 # PUBLISH_OUTPUT_DIRECTORY should point to the output directory of your dotnet publish command.
 # One example is /path/to/your/csproj/bin/Release/netcoreapp3.1/osx-x64/publish/.
 # If you want to change output directories, add `--output /my/directory/path` to your `dotnet publish` command.
-INFO_PLIST="/Users/bluesky/Workspace/Projects/No-Front/NAS-AutoHelper/NAS-AutoHelper/_mac_publish/Info.plist"
-ICON_FILE="/Users/bluesky/Workspace/Projects/No-Front/NAS-AutoHelper/NAS-AutoHelper/_mac_publish/tray.icns"
+INFO_PLIST="./Info.plist"
+ICON_FILE="./tray.icns"
 ICON_FILE_NAME="tray.icns"
 
 if [ -d "$APP_NAME" ]
