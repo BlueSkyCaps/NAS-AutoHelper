a56f1bbef6ee5de2620944cb11018473a0e3caf0
bluesky <linsongxian@outlook.com>
2025-01-19 16:25:54 +0800

    注释

--- UPS-AutoHelper/_mac_publish/macapp.sh
+++ UPS-AutoHelper/_mac_publish/macapp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 APP_NAME="./UPS自动关机.app"
+# 替换成dotnet编译好的目录
 PUBLISH_OUTPUT_DIRECTORY="../bin/Release/net8.0/osx-arm64/publish/."
 # PUBLISH_OUTPUT_DIRECTORY should point to the output directory of your dotnet publish command.
 # One example is /path/to/your/csproj/bin/Release/netcoreapp3.1/osx-x64/publish/.
